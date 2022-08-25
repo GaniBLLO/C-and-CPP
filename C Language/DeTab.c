c70713744573987610917e613ebb2a54051af2ac
Sokolov Eugene <zm-7@inbox.ru>
2022-08-25 17:26:36 +0300

    DeTab

---
+++ C Language/DeTab.c
@@ -0,0 +1,31 @@
+﻿/*Program name: Detab.
+In this program SPACEs == TABs. Change tabs('\t') by spaces(' ')*/
+#include<stdio.h>
+#define SPACES	4		
+
+bool ReadChar(int& c)
+{
+	char k = ' ';
+	c = getchar();					//Read input symb.
+	{
+		if (c == '\t')				//If symb == '\t'
+		{
+			for (int i = 0; i < SPACES - 1; i++)
+				{
+					printf("%s", k);
+				}
+		}
+		else
+			return c != EOF;
+	}
+}
+
+int main()
+{
+	int c;
+	while (ReadChar(c))
+	{
+		putchar(c);
+	}
+	return 0;
+}
