39da3e36a3b4b03c7821910e5190ba35593fe60e
Sokolov Eugene <zm-7@inbox.ru>
2022-10-24 13:54:38 +0300

    Done

--- C Language/Squeeze.c
+++ C Language/Squeeze.c
@@ -1,5 +1,7 @@
-﻿/*Program name: squeezek.
-In this program s2 delete all removes what appears in the string s1*/
+﻿/*Program name: squeezek & any.
+Squeezek = In this program s2 delete all removes what appears in the string s1
+Any = analog strpbrk*/
+
 #include<stdio.h>
 
 void squeezek(char s1[], const char s2[])
@@ -16,9 +18,24 @@ void squeezek(char s1[], const char s2[])
 
 }
 
+int any(const char s1[], const char s2[])
+{
+    int i, j;
+    if(!*s2)
+        return -1;
+    for (j = i = 0; s1[i] != '\0'; i++)
+        {
+        if (s1[i] == *s2)
+            return i+1;
+        }
+    return -1;
+}
+
 int main()
 {
     char w_1[] = { "Hello_dude_how_are_you?" }, w_2[] = {"_dude"};
     squeezek(w_1, w_2);
+	int result = any(w_1, w_2);
+    printf("%d", result);
 	return 0;
 }
