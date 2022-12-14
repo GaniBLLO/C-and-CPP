e11491c2ed326c24b6184d0f75c29db292e1fe55
Sokolov Eugene <zm-7@inbox.ru>
2022-12-14 17:14:36 +0300

    Not Yet

--- C Language/DeTab.c
+++ C Language/DeTab.c
@@ -1,32 +1,38 @@
-﻿/*Program name: Detab.
-In this program SPACEs == TABs. Change tabs('\t') by spaces(' ')*/
-#include<stdio.h>
+/*Task 1.2: Write a program detab that replaces tabs in the input with the proper number of blanks to space to the next tab stop. 
+Assume a fixed set of tab stops, say every n columns.*/
+int SPACES = 8;
 
-#define SPACES	4		
+int detab(int character) {
+    int i = 0;
+    extern int SPACES;
+    
+    if (character == -1)
+        return -1;
+    else if (character == '\t') {
+        while (SPACES-- != 0)
+            putchar('_');
+        SPACES = 8;
+        return 0;
+    }
 
-bool ReadChar(int& c)
-{
-	c = getchar();										//Read input symb.
-	{
-		if (c == '\t')									//If symb == '\t'
-		{
-			printf("%4s", c);
-			//for (int i = 0; i < SPACES - 1; i++)		
-			//	{
-					
-			//	}
-		}
-		else
-			return c != EOF;
-	}
+    if (SPACES != 0)
+        SPACES--;
+    else
+        SPACES = 7;
+    return putchar(character);
 }
+
+/*Task 1.2.1:Write a program entab that replaces strings of blanks by the minimum number of tabs and blanks to achieve the same spacing. 
+Use the same tab stops as for detab.*/
 
 int main()
 {
 	int c;
-	while (ReadChar(c))
-	{
-		putchar(c);
-	}
-	return 0;
+    extern int SPACES;
+    while ((c = getchar()) != EOF) {
+        detab(c);
+        if (c == '\n')
+            SPACES = 8;
+    }
+    return 0;
 }
