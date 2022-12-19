3a7ceb26d900043ca9556bf94e3b8d24162702ef
Sokolov Eugene <zm-7@inbox.ru>
2022-12-19 17:13:41 +0300

    Add gpio.c

---
+++ Core/Inc/GPIO_code.h
@@ -0,0 +1,16 @@
+/*
+ * GPIO.h
+ *
+ *  Created on: Dec 19, 2022
+ *      Author: Sokolov EvgenII
+ */
+#include "stm32f1xx_hal.h"
+
+#ifndef INC_GPIO_CODE_H_
+#define INC_GPIO_CODE_H_
+
+void GPIO_Init(void);
+static void MX_GPIO_Init(void);
+
+
+#endif /* INC_GPIO_CODE_H_ */
