cb5e57144952912af0d2811971e2042dabd54656
Sokolov Eugene <zm-7@inbox.ru>
2023-05-30 16:35:47 +0300

    Work with private and public classes using functions and working with vectors

---
+++ YA_W2/W3_Classes/CPPWhite.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+class SortedString {
+public:
+    void AddString(const std::string& s) {
+        sorted.push_back(s);
+    }
+    std::vector<std::string> GetSortedString() {
+        std::sort(sorted.begin(), sorted.end());
+        return sorted;
+    }
+private:
+    std::vector<std::string> sorted;
+};
+
+void PrintSortedStrings(SortedString& strings) {
+    for (const std::string& s : strings.GetSortedString()) {
+        std::cout << s << " ";
+    }
+    std::cout << "\n";
+}
+
+int main()
+{
+ 
+//classes
+    std::vector<std::string> my_name = { "fire", "first", "second", "second", "third", "fire", "loop", "data", "camp"};
+    SortedString my_class_n;
+    for (auto& x : my_name) {
+        my_class_n.AddString(x);
+    }
+    PrintSortedStrings(my_class_n);
+
+    return 0;
+}
